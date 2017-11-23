3216851ff4a3247c1de3478e89af16a3a0aea574
Juraj Kirchheim <back2dos@googlemail.com>
2017-11-23 13:31:52 +0100

    Add more tests.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -2,18 +2,20 @@ package ;
 
 import corsair.Middleware;
 import tink.http.Response;
+import travix.Logger.*;
 using tink.CoreApi;
 using tink.io.Source;
 
-import travix.Logger.*;
 
 class RunTests {
 
   static function main() {
     new tink.http.containers.NodeContainer(12345).run(
-      corsair.Middleware.create()(function (req) {
-        return Future.sync(OutgoingResponse.blob('hello, world', 'text/plain'));
-      })
+      corsair.Middleware.create({ paramName: 'forward-to' })(
+        corsair.Middleware.create()(function (req) {
+          return Future.sync(OutgoingResponse.blob('hello, world', 'text/plain'));
+        })
+      )
     );
 
     function get(url:String)
@@ -21,25 +23,43 @@ class RunTests {
         tink.http.Fetch.fetch('http://localhost:12345/$url')
           .next(function (res)
             return res.body.all()
-          )
-          .next(function (c) return c.toString());
+              .next(function (c) return {
+                header: res.header,
+                body: c.toString()
+              })
+          );
+    
+    function loc(param:String) {
+      var expected = 'http://localhost:12345/?$param=https://github.com/';
+      return get('?$param=http://github.com/')
+        .next(function (res) return res.header.byName(LOCATION))
+        .next(function (l) return 
+          if (l == expected)
+            true
+          else
+            new Error('Expected $expected but got $l')
+        );
+    }
 
-    get('')
+    loc('forward-to')
+      .next(function (_) return loc('proxy-to'))
+      .next(function (_) return get('foobar'))
       .next(
         function (hello)
-          return 
-            if (hello != 'hello, world') 
-              new Error('expected "hello, world" but got $hello')
-            else
-              get('?proxy-to=http://example.com/')
+          return switch hello.body {
+            case 'hello, world':
+              get('?proxy-to=http://example.com/');
+            case unexpected:
+              new Error('expected "hello, world" but got $unexpected');
+          }
       )
     .handle(function (o) switch o {
-      case Success(unexpected) if (unexpected.indexOf('<title>Example Domain</title>') == -1):
+      case Success({ body: unexpected }) if (unexpected.indexOf('<title>Example Domain</title>') == -1):
         println('received unexpected response:');
         println('');
         println(unexpected);
         exit(500);      
-      case Success(v):
+      case Success(_):
         println('It appears to have worked.');
         exit(0);
       case Failure(e):
